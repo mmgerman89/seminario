c451c78bfefcb5ab4c23f824c1f37825f8e497fe
German <mmgerman89@gmail.com>
2015-04-05 22:29:32 -0300

    precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
 //= require turbolinks
 //= require_tree .
 
