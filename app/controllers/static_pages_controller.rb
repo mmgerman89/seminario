4a538e982ff11c34c3c2542cab8922bca018316d
German <mmgerman89@gmail.com>
2015-04-07 15:23:40 -0300

    inicio arreglado

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if user_signed_in?
-    @users = Array.new    
-    User.where.not(id: current_user.id).each do |u|
-      if !current_user.friends.include? u
-        @users << u
+    if user_signed_in?
+      @micropost = current_user.microposts.build
+
+      @users = Array.new    
+      User.where.not(id: current_user.id).each do |u|
+        if !current_user.friends.include? u
+          @users << u
+        end
       end
+      
+      @commerces = Commerce.all
     end
-    
-    @commerces = Commerce.all
   end
 
   def commerces
