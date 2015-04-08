e3789a4d23d7f6f4df45f38683967a43a84334e0
German <mmgerman89@gmail.com>
2015-04-07 22:22:56 -0300

    arreglando moco

--- db/migrate/20150317215132_add_menu_id_to_branches.rb
+++ db/migrate/20150317215132_add_menu_id_to_branches.rb
@@ -2,13 +2,6 @@ class AddMenuIdToBranches < ActiveRecord::Migration
   def change
     add_column :branches, :menu_id, :integer
     
-    menus = Menu.all
-    
-    menus.each do |menu|
-      branch = Branch.find_by_id(menu.branch_id)
-      branch.menu_id = menu.id
-      branch.save!
-    end
   end
   
   def down
